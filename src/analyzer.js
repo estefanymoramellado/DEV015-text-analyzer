af5e4aa864a0589b85bddbd93720cc7898de97f9
noir.desir.sublime@gmail.com <tuemailexample.com>
2024-05-27 17:08:57 -0400

    Js 3 funciones

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,7 +1,21 @@
-const analyzer = {  
-  getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+
+const analyzer = { //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+ 
+ getCharacterCount: function (text) {
+    return text.length; 
   },
+  getCharacterCountExcludingSpaces: function (text) { //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `
+    const caracteresSinEspacios = text.replace(/[^a-zA-Z0-9]/g, "");
+    return caracteresSinEspacios.length;
+  },
+  getWordCount: function (text) {  //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const palabras = text.trim().split(/\s+/);
+    return palabras.filter(palabra => palabra.length > 0).length;
+};
+}    
+
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
@@ -17,6 +31,6 @@ const analyzer = {
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
-};
 
-export default analyzer;
+  export default analyzer;
+  
--- src/index.js
+++ src/index.js
@@ -1,3 +1,69 @@
-import analyzer from './analyzer.js';
+//import analyzer from './analyzer.js';
 
-//TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+document.addEventListener('DOMContentLoaded', function() {
+  const textarea = document.querySelector('textarea[name="user-input"]');
+  const caracteresLi = document.querySelector('.caracteres[data-caracteres="0"]');
+  const caracteresSinLi = document.querySelector('.caracteres[data-caracteres-sin="0"]');
+  const palabrasLi = document.querySelector('.caracteres[data-palabras="0"]');
+  const numerosLi = document.querySelector('.numeros[data-palabras="0"]');
+
+  textarea.addEventListener('input', function() {
+    const texto = textarea.value;
+
+    const caracteres = texto.length;
+    caracteresLi.textContent = 'Caracteres: ' + caracteres;
+
+    const caracteresSinEspacios = texto.replace(/[^a-zA-Z0-9]/g, "");
+    const caracteresSinEspaciosLength = caracteresSinEspacios.length;
+    caracteresSinLi.textContent = 'Caracteres sin espacios: ' + caracteresSinEspaciosLength;
+
+    const palabras = texto.trim().split(/\s+/);
+    const palabrasLength = palabras.filter(palabra => palabra.length > 0).length;
+    palabrasLi.textContent = 'Palabras: ' + palabrasLength;
+
+  });
+});
+
+
+
+
+//document.addEventListener('DOMContentLoaded', function() {
+ // const textarea = document.querySelector('textarea[name="user-input"]');
+
+//textarea.addEventListener('input', function() {
+
+  //const texto = textarea.value;
+ // const cleanedTest = texto.replace(/[^a-zA-Z0-9]/g, "");
+  //const caracteresSinEspacios = cleanedTest.length
+
+  //caracteresSinLi.textContent = 'Caracteres sin espacios: ' + caracteresSinEspacios;
+
+  //});
+
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const string="  Esto    es  un    texto con    varios    espacios    ";
+  //string.replace(/\s+/g, " "); // "  Esto es un texto con varios espacios    "
+  //string.replace(/\s+/g, " ").trim(); // "Esto es un texto con varios espacios"
+//const input = document.getElementById("input");
+ // input.value.trim();
+
